d854efe022edf59ac5dfca8b3ed4611d60f84446
stefano.peraldini <stefano.peraldini@hackaboss.com>
2025-05-26 10:58:29 +0200

    Constructor

--- app/src/entity/Alumno.java
+++ app/src/entity/Alumno.java
@@ -6,4 +6,10 @@ public class Alumno {
     private int id;
     private String nombre;
     private LocalDate fechaDeNacimiento;
+
+    public Alumno(int id, String nombre, LocalDate fechaDeNacimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
 }
